e8ab54de11c63c94e7aeb4e9800d37ac896cc868
Pratyaksh Rikhari <pratyaksh@plantd.life>
2024-07-20 14:11:43 +0530

    Changes

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Pratyaksh Rikhari on 20/07/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
